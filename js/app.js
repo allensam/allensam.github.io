3031063ea495fc8d4ce6078dfdf605f13ccae0ae
Sam Allen <allensam0@gmail.com>
2015-10-20 09:37:49 -0600

    maybe?

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ website.run(function ($state, $rootScope, $window) {
 
 website.config(function ($stateProvider, $urlRouterProvider, $locationProvider) { //enables html5 so that routing looks better
 
-  $locationProvider.html5Mode(true).hashPrefix('!');
+  $locationProvider.html5Mode(true);
 
   //calles the stateProvider which is basically a thing that handles the state of the html (what page it is on)
 
